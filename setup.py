1e23524483b1a7fc42b5a02624e713cc5d04f673
David THENON <sveetch@gmail.com>
2013-08-17 17:35:32 +0200

    Update setup.py, bumping to minor version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=[
-        'django-crispy-forms==1.2.7',
+        'django-crispy-forms >= 1.2, <=1.3.2'
     ],
     include_package_data=True,
     zip_safe=False
