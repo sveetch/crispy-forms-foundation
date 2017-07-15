ea089d3edb0a339e34785752f9e37eb2cd22720b
David THENON <sveetch@gmail.com>
2017-07-15 15:53:52 +0200

    Added tests for ButtonElement, bump to 0.6.3 pre release 5

--- crispy_forms_foundation/__init__.py
+++ crispy_forms_foundation/__init__.py
@@ -1,2 +1,2 @@
 """Django application to add 'django-crispy-forms' layout objects for 'Foundation for sites'"""  # noqa: E501
-__version__ = '0.6.3-pre.4'
+__version__ = '0.6.3-pre.5'
--- project_test/tests/004_buttons.py
+++ project_test/tests/004_buttons.py
@@ -3,10 +3,11 @@ import os
 import pytest
 
 from crispy_forms_foundation.layout import (Layout, ButtonGroup,
-                                            Submit, Button)
+                                            Submit, Button,
+                                            ButtonElement, SubmitButton)
 
 from project_test.tests.forms import BasicInputForm
-#from project_test.tests.utils import write_output
+from project_test.tests.utils import write_output
 
 
 def test_buttongroup(output_test_path, render_output, rendered_template,
@@ -27,5 +28,24 @@ def test_buttongroup(output_test_path, render_output, rendered_template,
     attempted = render_output(os.path.join(output_test_path, pack,
                                            "test_buttongroup.html"))
     #write_output(output_test_path, pack, "test_buttongroup.html", rendered)
+
+    assert attempted == rendered
+
+
+def test_buttonelement(output_test_path, render_output, rendered_template,
+                     helper, client):
+    form = BasicInputForm()
+    pack = helper.template_pack
+
+    helper.layout = Layout(
+        SubmitButton('Save', 'Save'),
+        ButtonElement('Foo', 'Foo'),
+    )
+
+    rendered = rendered_template(form, helper=helper)
+
+    attempted = render_output(os.path.join(output_test_path, pack,
+                                           "test_buttongroup.html"))
+    write_output(output_test_path, pack, "test_buttonelement.html", rendered)
 
     assert attempted == rendered
